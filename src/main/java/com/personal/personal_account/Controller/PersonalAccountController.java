19b53ea2cc6cd932e434d99142a99174363c28e2
Ten Aleksandr Aleksandrovich <89169604848t@gmail.com>
2024-03-16 13:49:41 +0300

    Added jaeger trace for getting personal data

--- src/main/java/com/personal/personal_account/Controller/PersonalAccountController.java
+++ src/main/java/com/personal/personal_account/Controller/PersonalAccountController.java
@@ -2,6 +2,9 @@ package com.personal.personal_account.Controller;
 
 import com.personal.personal_account.Model.User;
 import com.personal.personal_account.Service.AccountService;
+import io.opentracing.Span;
+import io.opentracing.Tracer;
+import io.opentracing.tag.Tags;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,8 +13,13 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class PersonalAccountController {
     private final AccountService accountService;
+    private final Tracer tracer;
     @GetMapping("/account/{id}")
     public User getPersonalData(@PathVariable Long id){
+        Span span = tracer.buildSpan("Getting personal data").start();
+        Tags.HTTP_METHOD.set(span, "GET");
+        Tags.HTTP_URL.set(span, "/api/personal/account/"+id);
+        span.finish();
         User user = accountService.getUserInfo(id);
         return user;
     }
