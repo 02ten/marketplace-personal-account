93a3d0db7c38b24f5c1d3d15de2ea7acb74f4e2c
Ten Aleksandr Aleksandrovich <89169604848t@gmail.com>
2024-03-16 13:37:26 +0300

    Deleted UserDTO

--- src/main/java/com/personal/personal_account/DTO/UserDTO.java
+++
@@ -1,9 +0,0 @@
-package com.personal.personal_account.DTO;
-
-import java.util.List;
-
-public class UserDTO {
-    private Long id;
-    private String firstName;
-    private List<String> roles;
-}
